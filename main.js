0be7c3697c3f1f6386c330cf93cdb2487a5b1013
windspinks <windspinks@gmail.com>
2017-06-15 11:59:55 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,64 @@
+const audioPlayer = document.querySelector('.music-player');
+const client_id = "095fe1dcd09eb3d0e1d3d89c76f5618f";
+const searchButton = document.querySelector('.searchButton');
+const searchField = document.querySelector('.searchField');
+const nowPlaying = document.querySelector('.nowPlaying');
+const nowPlayingUser = document.querySelector('.nowPlayingUser');
+const results = document.querySelector('.results');
+let trackList;
+
+SC.initialize({
+  client_id: '095fe1dcd09eb3d0e1d3d89c76f5618f'
+});
+
+//Find all tracks of inputted name, limit of 15, store in trackList
+function searchArtist(artistName) {
+  SC.get('/tracks', {
+    limit: 15,
+    q: artistName,
+    license: 'cc-by-sa'
+  }).then(function(tracks) {
+    console.log(tracks);
+    trackList = tracks;
+
+    styleResults(trackList);
+  });
+}
+
+function playTrack(trackNum){
+  track = trackList[trackNum];
+  if (track.streamable){
+    audioPlayer.src = track.stream_url + "?client_id=" + client_id;
+    nowPlaying.innerHTML = track.title;
+    nowPlayingUser.innerHTML = track.user.permalink;
+  }
+  else {
+    window.alert("Track is not streamable, please select another");
+  }
+}
+
+function styleResults(trackList){
+  for (let i = 0; i < trackList.length; i++){
+    let currentTrack = trackList[i];
+  }
+}
+
+searchField.addEventListener('keydown', function(e) {
+  if (e.keyCode === 13){
+    e.preventDefault();
+    searchArtist(searchField.value);
+    searchField.value = "";
+  }
+});
+
+searchButton.addEventListener('click', function(e) {
+  searchArtist(searchField.value);
+  searchField.value = "";
+});
+
+
+
+
+// 4. Create a way to append the fetch results to your page
+
+// 5. Create a way to listen for a click that will play the song in the audio play
